57a418f786ed1898a227fe957562422ab7e1b125
Viktor.Markvardt <viktor.markvardt@gmail.com>
2022-12-20 18:20:44 +0200

    Moved some functions to Unils

--- UserApp/Utils.cpp
+++ UserApp/Utils.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <xpxchaincpp/utils/HexParser.h>
 #include <utils/HexFormatter.h>
+#include <QMessageBox>
 #include "Utils.h"
 
 void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
@@ -48,14 +49,70 @@ void customMessageHandler(QtMsgType type, const QMessageLogContext &context, con
 }
 
 
-QString rawHashToHex(const std::array<uint8_t, 32>& rawHash) {
+QString rawHashToHex(const std::array<uint8_t, 32>& rawHash)
+{
     std::ostringstream stream;
     stream << sirius::utils::HexFormat(rawHash);
     return stream.str().c_str();
 }
 
-std::array<uint8_t, 32> rawHashFromHex(const QString& hex) {
+std::array<uint8_t, 32> rawHashFromHex(const QString& hex)
+{
     std::array<uint8_t, 32> hash{};
     xpx_chain_sdk::ParseHexStringIntoContainer(hex.toStdString().c_str(), hex.size(), hash);
     return hash;
-};
+}
+
+QString getResource( const QString& resource )
+{
+#ifdef __APPLE__
+    return QCoreApplication::applicationDirPath() + "/." + resource;
+#else
+    return resource;
+#endif
+}
+
+bool isFolderExists(const std::string& path)
+{
+    return std::filesystem::exists( path ) || ! std::filesystem::is_directory( path );
+}
+
+std::filesystem::path getSettingsFolder()
+{
+    std::filesystem::path path;
+#ifdef _WIN32
+    qDebug() << LOG_SOURCE << "!NOT IMPLEMNTED! FOR WIN32: settingsPath()";
+    exit(1);
+#else
+    const char* homePath = getenv("HOME");
+    path = std::string(homePath) + "/.XpxSiriusStorageClient";
+#endif
+
+    std::error_code ec;
+    if ( ! std::filesystem::exists( path, ec ) )
+    {
+        std::filesystem::create_directories( path, ec );
+        if ( ec )
+        {
+            QMessageBox msgBox;
+            msgBox.setText( QString::fromStdString( "Cannot create folder: " + std::string(path) ) );
+            msgBox.setInformativeText( QString::fromStdString( ec.message() ) );
+            msgBox.setStandardButtons(QMessageBox::Ok);
+            msgBox.exec();
+            exit(1);
+        }
+    }
+
+    return path;
+}
+
+std::filesystem::path getFsTreesFolder()
+{
+    return getSettingsFolder() / "FsTrees";
+}
+
+QDebug& operator<<(QDebug& out, const std::string& str)
+{
+    out << QString::fromStdString(str);
+    return out;
+}
--- UserApp/Utils.h
+++ UserApp/Utils.h
@@ -1,13 +1,34 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#define LOG_SOURCE \
-    __FILE__ << __LINE__
+#include <iostream>
+#include <filesystem>
 #include <QMessageLogger>
 
+#define LOG_SOURCE \
+    __FILE__ << __LINE__
+
+#define ASSERT(expr) { \
+    if (!(expr)) {\
+        std::cerr << ": " << __FILE__ << ":" << __LINE__ << " failed assert: " << #expr << "\n" << std::flush; \
+        assert(0); \
+    }\
+}
+
 void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
+
 QString rawHashToHex(const std::array<uint8_t, 32>& rawHash);
+
 std::array<uint8_t, 32> rawHashFromHex(const QString& hex);
 
+QString getResource(const QString& resource);
+
+bool isFolderExists(const std::string& path);
+
+std::filesystem::path getSettingsFolder();
+
+std::filesystem::path getFsTreesFolder();
+
+QDebug& operator<<(QDebug& out, const std::string& str);
 
 #endif //UTILS_H
